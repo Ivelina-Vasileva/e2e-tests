e720da0a04f80c7503653d88062e8a03c3249447
Ivelina-Vasileva <ivchetovasileva@gmail.com>
2025-03-20 23:45:29 +0200

    Rework order page tests

--- e2e/order.spec.ts
+++ e2e/order.spec.ts
@@ -1,23 +1,15 @@
-import { test, expect } from '@playwright/test';
+import { test } from '@playwright/test';
+import { OrderPage } from './pages/OrderPage';
 
 test('Order', async ({ page }) => {
-  // 1. Зареди URL на сайта
-  await page.goto('https://solvewoo.com/');
-  const productLocator = page.locator('//*[@id="post-2"]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div/div[3]/div');
-  await productLocator.hover(); // 2. Хавърва на продукта
-  const buttonLocator = page.locator('//*[@id="post-2"]/div/div/div[2]/div/div/div/div/div[2]/div[2]/div/div[3]/div/div[3]/div[3]/div[2]/div[2]/a');
-  await buttonLocator.click(); // 3. Клика върху продукта
-  await page.getByRole('link', { name: 'Поръчка' }).click(); // 4. Клика на "Поръчка"
+  const orderPage = new OrderPage(page);
+  await orderPage.goto();
+  await orderPage.selectProduct();
+  await orderPage.addToCart();
   await page.waitForTimeout(2000)
-  await page.locator('//*[@id="billing_first_name"]').fill('Test User'); // 5. Име
-  await page.getByLabel('Фамилия').fill('Test User'); //6. Фамилия
-  const prefixes = ['087', '088', '089']; //7. Валидни телефонни префикси
-  const randomPrefix = prefixes[Math.floor(Math.random() * prefixes.length)]; //8. Генерира автоматично префикс
-  const phoneNumber = `${randomPrefix}${Math.floor(1000000 + Math.random() * 9000000)}`; // 9. Генерира телефонния номер
-  await page.getByLabel('Телефон').fill(phoneNumber); // 10. Въвежда телефонен номер
-  await page.getByLabel('Имейл адрес').fill(`testuser${Date.now()}@example.com`); //11. Въвежда имейл
-  await page.getByRole('button', { name: 'Поръчване' }).click(); //12.Клика на "Поръчване"
+  await orderPage.goToCheckout();
+  await orderPage.fillPersonalDetails('Test', 'User');
+  await orderPage.placeOrder();
 
-  // 13.Тук трябва да пише,че сме направили успешно поръчка :)
   await page.close();
 })
